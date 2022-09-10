b7011e861293a9e1b0b25ef26f5aebfc00848707
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2022-09-11 00:19:11 +0530

    Delete loop.java

--- loop.java
+++
@@ -1,9 +0,0 @@
-public class loop {
-    public static void main(String[] args) {
-        int i=0;
-        while(i<=10){
-            System.out.println("kanishk");
-            i++;
-        }
-    }
-}
